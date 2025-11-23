3e270e5ec45e15c2d8eb161160244c164ca7112a
Daniel-Elgarisi <danielelgarisi7@gmail.com>
2025-11-23 20:15:26 +0200

    Changes to pipline-seed to work with parameters

--- jenkins/init.groovy.d/02-pipeline-seed.groovy
+++ jenkins/init.groovy.d/02-pipeline-seed.groovy
@@ -3,6 +3,9 @@ import org.jenkinsci.plugins.workflow.job.WorkflowJob
 import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition
 
 import hudson.plugins.git.*
+import hudson.model.ParametersDefinitionProperty
+import hudson.model.ChoiceParameterDefinition
+import hudson.model.StringParameterDefinition
 
 println ">>> Initializing Git-based Pipeline job..."
 
@@ -13,34 +16,58 @@ def repoUrl = "https://github.com/Daniel-Elgarisi/ci-jenkins-infra.git"
 def scriptPath = "pipelines/sample-pipeline/Jenkinsfile"
 def credentialsId = "github-ci-token"
 
-if (instance.getItem(jobName) == null) {
 
+WorkflowJob job = instance.getItem(jobName) as WorkflowJob
+
+if (job == null) {
     println ">>> Creating pipeline job: ${jobName}"
 
-    def scm = new GitSCM(
-        [
-            new UserRemoteConfig(
-                repoUrl,
-                null,
-                null,
-                credentialsId
-            )
-        ],
-        [new BranchSpec("*/main")],
-        false,
-        Collections.emptyList(),
-        null,
-        null,
-        Collections.emptyList()
-    )
-
-    def job = new WorkflowJob(instance, jobName)
-    job.setDefinition(new CpsScmFlowDefinition(scm, scriptPath))
-
+    job = new WorkflowJob(instance, jobName)
     instance.add(job, jobName)
-    instance.save()
 
     println ">>> Created Git pipeline job: ${jobName}"
 } else {
-    println ">>> Pipeline job already exists: ${jobName}"
+    println ">>> Updating existing pipeline job: ${jobName}"
 }
+
+def scm = new GitSCM(
+    [
+        new UserRemoteConfig(
+            repoUrl,
+            null,
+            null,
+            credentialsId
+        )
+    ],
+    [new BranchSpec("*/main")],
+    false,
+    Collections.emptyList(),
+    null,
+    null,
+    Collections.emptyList()
+)
+
+def flowDefinition = new CpsScmFlowDefinition(scm, scriptPath)
+job.setDefinition(flowDefinition)
+
+
+def parameters = [
+    new ChoiceParameterDefinition(
+        "ENVIRONMENT",
+        ["dev", "staging", "prod"] as String[],
+        "Target environment to deploy to"
+    ),
+    new StringParameterDefinition(
+        "IMAGE_TAG",
+        "latest",
+        "Docker image tag or application version"
+    )
+]
+
+job.removeProperty(ParametersDefinitionProperty) 
+job.addProperty(new ParametersDefinitionProperty(parameters))
+
+job.save()
+instance.save()
+
+println ">>> Pipeline job configured with parameters ENVIRONMENT and IMAGE_TAG"
